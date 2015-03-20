36d0188d2b2964e28e8e707fc040156c46ada7f9
Tole <Tole>
2015-03-20 01:20:56 -0700

    serve fix

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var morgan = require('morgan');
 var app = express();
-
+var bodyParser = require('bodyParser');
+var methodOverride - require('method-override');
 // app.get('/', function(res, req) {
 //   res.sendfile(__dirname + '/client/views/index.html')
 // })
@@ -9,6 +10,11 @@ var port = process.env.PORT || 8000;
 
 app.use(express.static(__dirname + '/client/views/'));
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(bodyParser.json());
+app.use(methodOverride())
 app.get('/', function(req, res) {
   app.get('index.html')
 })
